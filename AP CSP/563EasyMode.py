d7d3fc5911a6a1234e8745f5bbdfd67e4e143283
marsh135 <86257034+marsh135@users.noreply.github.com>
2026-02-10 11:53:26 -0500

    Update 563EasyMode.py

--- AP CSP/563EasyMode.py
+++ AP CSP/563EasyMode.py
@@ -7,7 +7,7 @@ Description: This is intended to help you get started on 5.6.3
 score = 0
 
 
-#Make a list, called trivia, of AT LEAST 10 dictionaries. Each dictionare should follow the syntax of:
+#Make a list, called trivia, of AT LEAST 10 dictionaries. Each dictionary should follow the syntax of:
 # {"q":"ENTER YOUR QUESTION HERE", "a":"ANSWER"}
 #note the "q":"" , "a":"" pairs.  This allows you to CONSISTENTLY access the question using the "q" key and the answer using the 
 #"a" key.  Your answers should be either ALL CAPS or all lowers to make validating correct answers later easier
@@ -18,7 +18,7 @@ trivia =[
      {"q": "SPORTS:In basketball, how many points is a free throw worth? " , "a":"1"},
      {"q": "CARS: Which part of the car us used to disconnect the engine from the tranmission in a manual transmission" , "a":"clutch"},
      {"q": "CARS: Which dashboard gauge is used to show how fast the engine in spinning in RPMs" , "a":"tachometer"},
-     {"q": "CARS: What is a common component ona  gasoline engine that a diesel engine does not use? " , "a":"spark plugs"}, 
+     {"q": "CARS: What is a common component on a gasoline engine that a diesel engine does not use? " , "a":"spark plug"}, 
      {"q": "FOOD:This fruit is the main ingredient in guacamole: " , "a":"avocado"},
      {"q": "FOOD: This italian dish is made from dough, sauce, and cheese: " , "a":"pizza"},
      {"q": "FOOD: What is the byproduct made when fat is extracted from cream to make butter? " , "a":"buttermilk"},     
@@ -46,7 +46,7 @@ def main():
             print()
 
             score = ask(trivia) #call the ask function, written below. Return the score as a value out of the len of trivia
-            leaderboard[name] =  score
+            leaderboard[name] =  score  # add the key value pair to the dictionary. Name is key, score is value
 
         #quit game case    
         elif choice == "Q":
@@ -62,18 +62,21 @@ def main():
   
 def ask(trivia):
   global score
+  score = 0
   global qs
-  for q in trivia:
-    print(q["q"])
+  for q in trivia: #each q is a list tiem, in this case, a question dictionary
+    print(q["q"]) #print whatever is in that dictionarya t key "q"
     user = input("ANSWER:  ").strip().lower() #all input will be forced to lower case, which matches out "a":"answer" above
-    if user == q["a"]:
+    if user == q["a"]:  #check to see if the user input matches the contents of the dictionary at key "a"
       print("Correct!")
-      print()
+      print() #pretty
       score+=1
     else:
-      print("Incorrect. The answer was:" , q["a"] +".")
-      print()
-  print(f"Score: {score}   Percentage:{score/qs*100:.2f}%")
-  return score
+      print("Incorrect. The answer was:" , q["a"] +".")  #this does NOT account for spelling mistakes, plurals, etc. If it doesn't match EXACTLY, it is wrong
+      print() #pretty
+  print(f"Score: {score}   Percentage:{score/qs*100:.2f}%") #show the score. Using an f string for simplicity
+  return score #score comes back as an integer value, which we use above
             
-main()
+main()  # invokes the main method, or function.  VERY common in python and other programming languages
+# if fact, Java programs CANNOT execute without the main method. 
+#public static void main(String[] args){}
